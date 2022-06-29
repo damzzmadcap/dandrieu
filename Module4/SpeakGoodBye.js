b61f4a346418d5f0eb58331d531644a55764d441
damzzmadcap <104556568+damzzmadcap@users.noreply.github.com>
2022-06-29 17:53:31 +0200

    Add files via upload

---
+++ Module4/SpeakGoodBye.js
@@ -0,0 +1,9 @@
+(function speak(name) { 
+var byeSpeaker = {}; 
+byeSpeaker.name = "John", "Jen", "Jason", "Jim"; 
+var speakWord = "Good Bye ";
+byeSpeaker.sayGoodBye = function () { 
+console.log(speakWord + byeSpeaker.name); 
+} 
+name.byeSpeaker = byeSpeaker 
+})(name); 
---
+++ Module4/SpeakHello.js
@@ -0,0 +1,10 @@
+(function speak(name) {
+var helloSpeaker = {}; 
+helloSpeaker.name = "Yaakov", "Paul", "Frank", "Larry", "Paula", "Laura";
+var speakWord = "Hello ";
+helloSpeaker.sayHello = function () { 
+ console.log(speakWord + helloSpeaker.name); 
+} 
+name.helloSpeaker = helloSpeaker 
+
+})(name); 
