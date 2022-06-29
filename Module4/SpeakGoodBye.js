d976bded829554b71cee051f087b3170f8400265
damzzmadcap <104556568+damzzmadcap@users.noreply.github.com>
2022-06-30 00:04:22 +0200

    Delete SpeakGoodBye.js

--- Module4/SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-(function speak(name) { 
-var byeSpeaker = {}; 
-byeSpeaker.name = "John", "Jen", "Jason", "Jim"; 
-var speakWord = "Good Bye ";
-byeSpeaker.sayGoodBye = function () { 
-console.log(speakWord + byeSpeaker.name); 
-} 
-name.byeSpeaker = byeSpeaker 
-})(name); 
